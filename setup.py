c9a94b13f8850bc52a41725d78cc3472966aebeb
Matthias Kestenholz <mk@feinheit.ch>
2016-11-12 12:08:06 +0100

    django-cte-forest 0.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-cte-forest',
-    version='0.1.0',
+    version='0.2.0',
     packages = find_packages(),
     maintainer = 'Matthias Kestenholz',
     maintainer_email = 'mk@feinheit.ch',
